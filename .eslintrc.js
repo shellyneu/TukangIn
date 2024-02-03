6746fe77ae08e38f7a4c272500c39e6c6025be7b
Sheren Aura Vi <sherenaura21@gmail.com>
2024-02-04 06:08:55 +0700

    setup lint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,9 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    // ... aturan lainnya ...
+  },
 };
--- .prettierrc.js
+++ .prettierrc.js
@@ -4,4 +4,7 @@ module.exports = {
   bracketSpacing: false,
   singleQuote: true,
   trailingComma: 'all',
+  semi: false,
+  singleQuote: true,
+  jsxSingleQuote: true,
 };
