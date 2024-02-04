bef851254d4f4400ab51817d861f4568bf552165
Sheren Aura Vi <sherenaura21@gmail.com>
2024-02-04 20:48:26 +0700

    create ui for simpan screen

--- src/screens/simpan/index.js
+++ src/screens/simpan/index.js
@@ -1,11 +1,39 @@
-import {View, Text} from 'react-native'
-import React from 'react'
+import {View, Text, ScrollView} from 'react-native'
+import React, {useState} from 'react'
 import styles from './styles'
+import {Color, FontSize, Fonts} from '../../constants'
+import {NotificationIcon, SearchBar, TabBar} from '../../components'
 
 const SimpanScreen = () => {
+  const [activeTab, setActiveTab] = useState(1)
+
   return (
     <View style={styles.mainBody}>
-      <Text>SimpanScreen</Text>
+      {/* HEADER */}
+      <View style={styles.header}>
+        <View style={{gap: 4}}>
+          <Text style={styles.greetings}>Hi, Agus Setiawan</Text>
+          <Text style={styles.desc}>Yang Anda Simpan</Text>
+        </View>
+        <NotificationIcon />
+      </View>
+
+      {/* SEARCH */}
+      <SearchBar placeholder={'Cari order...'} />
+
+      {/* TAB BAR */}
+      <TabBar
+        tabs={[
+          {title: 'Pekerjaan', onPress: () => setActiveTab(1)},
+          {title: 'Tukang', onPress: () => setActiveTab(2)},
+        ]}
+        activeTab={activeTab}
+      />
+
+      {/* CONTENT */}
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <View></View>
+      </ScrollView>
     </View>
   )
 }
--- src/screens/simpan/styles.js
+++ src/screens/simpan/styles.js
@@ -2,5 +2,25 @@ import {StyleSheet} from 'react-native'
 import {FontSize, Fonts, Color} from '../../constants'
 
 export default StyleSheet.create({
-  mainBody: {backgroundColor: Color.BGSCREEN, flex: 1, paddingHorizontal: 16},
+  mainBody: {
+    backgroundColor: Color.BGSCREEN,
+    flex: 1,
+    paddingHorizontal: 16,
+    paddingTop: 15,
+  },
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  greetings: {
+    fontSize: FontSize.dp_12,
+    fontFamily: Fonts.REGULAR,
+    color: Color.GREYONE,
+  },
+  desc: {
+    fontSize: FontSize.dp_18,
+    fontFamily: Fonts.MEDIUM,
+    color: Color.BLACK,
+  },
 })
